a7c0f76cdade2fd6916dea96cb038f8a608e3c41
nlb4114 <nback24@gmail.com>
2020-02-18 03:02:44 +0000

    in class

--- dice.rb
+++ dice.rb
@@ -3,6 +3,21 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
+# get "/" do
+#   "Hello"
+# end
+
 get "/" do
-  "Hello"
+    @d1 = rand(1..6)
+    @d2 = rand(1..6)
+    # roll = d1 + d2
+    view "dice"
 end
+# if roll == 7 || roll == 11
+#     "WINNER!"
+# elsif roll == 2 || roll == 3 || roll == 12
+#     "YOU LOSE!"
+# elsif roll == 1 || roll == 4 || roll == 5 || roll == 6 || roll == 8 || roll == 9 || roll == 10
+#     "THE POINT IS #{roll}"
+# end
+
--- where2.rb
+++ where2.rb
@@ -4,6 +4,8 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  # lat: ± 90
-  # long: ± 180
+  @lat = rand(-90.000000..90.000000)
+  @long = rand(-180.000000...180.000000)
+  @lat_long = "#{@lat}, #{@long}"
+  view "where2"
 end
